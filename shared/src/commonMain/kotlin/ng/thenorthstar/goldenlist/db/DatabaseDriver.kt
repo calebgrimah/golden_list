92552ff1b4ca5ae2cce2c7130bea7dcf4f579227
Caleb Grimah <lamahgrimah@gmail.com>
2023-07-13 10:09:04 +0100

    Update DatabaseDriver.kt

--- shared/src/commonMain/kotlin/ng/thenorthstar/goldenlist/db/DatabaseDriver.kt
+++ shared/src/commonMain/kotlin/ng/thenorthstar/goldenlist/db/DatabaseDriver.kt
@@ -4,3 +4,6 @@ import com.squareup.sqldelight.db.SqlDriver
 import org.koin.core.scope.Scope
 
 internal expect fun Scope.createDriver(databaseName: String): SqlDriver
+
+
+//A new commment for the pr
